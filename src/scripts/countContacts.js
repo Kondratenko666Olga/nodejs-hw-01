6bd93e7714e9fc87ff94d203959e181ed21b1ab5
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-07-21 16:14:47 +0300

    countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from "node:fs/promises";
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, "utf-8");
+        const contacts = JSON.parse(data);
+        const counts = contacts.length;
+        console.log("Total contacts is:", counts);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 console.log(await countContacts());
