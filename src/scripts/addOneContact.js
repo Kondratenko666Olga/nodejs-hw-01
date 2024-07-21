53b7b0509694e959c6665cd527e766690eb11174
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-07-21 15:53:02 +0300

    addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,7 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from "node:fs/promises";
+import { createFakeContact } from "../utils/createFakeContact.js";
 
 export const addOneContact = async () => {
-    
+    try {
+        const data = await fs.readFile(PATH_DB, "utf-8");
+        const parsedData = JSON.parse(data);
+
+        const newContact = createFakeContact();
+
+        parsedData.push(newContact);
+
+        await fs.writeFile(PATH_DB, JSON.stringify(parsedData, null, 2));
+        console.log("Succesful pushed:", newContact);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 addOneContact();
