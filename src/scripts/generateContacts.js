23ca38ee6bb5f2e4efc9a7867010d1e86b5615c2
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-07-21 15:43:03 +0300

    generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,19 @@
+import fs from "node:fs/promises";
+import {createFakeContact} from "../utils/createFakeContact.js";
 import { PATH_DB } from '../constants/contacts.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+    try {
+        const data = await fs.readFile(PATH_DB, "utf-8");
+        const parsedData = JSON.parse(data);
+        for (let index = 1; index <= number; index++) {
+            const contact = createFakeContact();
+            parsedData.push(contact);
+        }
+        await fs.writeFile(PATH_DB, JSON.stringify(parsedData, null, 2));
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 generateContacts(5);
