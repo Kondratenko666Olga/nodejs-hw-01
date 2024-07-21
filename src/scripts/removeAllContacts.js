8a318d345e66d1838c9a13088e9f4ec543955a04
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-07-21 16:45:05 +0300

    removeAllContacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from "node:fs/promises";
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+    try {
+        const emptyContacts = [];
+
+        await fs.writeFile(PATH_DB, JSON.stringify(emptyContacts, null, 2));
+        console.log("Contacts deleted");
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 removeAllContacts();
